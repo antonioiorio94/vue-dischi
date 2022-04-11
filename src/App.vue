2adb5adc132e2e23d530da2fd30cf39b42d7a440
Antonio Iorio <antonioiorio94@gmail.com>
2022-04-11 15:10:57 +0200

    aggiunta header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <AppHeader />
+    <RecordList />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import AppHeader from "@/components/AppHeader.vue";
+import RecordList from "@/components/RecordList.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    AppHeader,
+    RecordList,
   },
 };
 </script>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <img src="@/assets/spotify-logo.png" alt="Spotyfy Logo" />
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+  props: {},
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/style/variables";
+
+header {
+  background-color: $main-color;
+  img {
+    width: 5rem;
+    padding: 10px;
+  }
+}
+</style>
